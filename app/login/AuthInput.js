beaf1c6718ead03f54b292c26404f59af882374e
Mrinal <cmrinal16@gmail.com>
2024-02-02 11:32:32 -0500

    update card view in expenses view

--- app/login/AuthInput.js
+++ app/login/AuthInput.js
@@ -3,15 +3,25 @@
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import secureLocalStorage from "react-secure-storage";
-import { TextField, Button, Container, Paper, Typography } from "@mui/material";
+import {
+  TextField,
+  Button,
+  Container,
+  Paper,
+  Typography,
+  CircularProgress,
+} from "@mui/material";
 
 export function AuthInput({ handleLogin }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
 
   const handleSubmit = async () => {
+    setLoading(true);
     let user = await handleLogin(email, password);
+    setLoading(false);
     if (user) {
       secureLocalStorage.setItem("email", email);
       secureLocalStorage.setItem("password", password);
@@ -23,54 +33,66 @@ export function AuthInput({ handleLogin }) {
   };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <Paper
-        elevation={3}
-        style={{
-          padding: 20,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Typography component="h1" variant="h5">
-          Login
-        </Typography>
-        <form style={{ width: "100%", marginTop: 20 }}>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button
-            fullWidth
-            variant="contained"
-            color="primary"
-            onClick={handleSubmit}
-            style={{ marginTop: 20 }}
-          >
+    <div>
+      <Container maxWidth="xs">
+        <Paper
+          elevation={3}
+          style={{
+            padding: 20,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h5">
             Login
-          </Button>
-        </form>
-      </Paper>
-    </Container>
+          </Typography>
+          <form style={{ width: "100%", marginTop: 20 }}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              onClick={handleSubmit}
+              style={{ marginTop: 20 }}
+            >
+              Login
+            </Button>
+          </form>
+          {loading && (
+            <div>
+              <CircularProgress size="large" />
+            </div>
+          )}
+        </Paper>
+      </Container>
+      {loading && (
+        <div>
+          <CircularProgress size="large" />
+        </div>
+      )}
+    </div>
   );
 }
--- app/view/ExpensesView.js
+++ app/view/ExpensesView.js
@@ -6,8 +6,6 @@ import {
   Button,
   Container,
   Grid,
-  Typography,
-  Modal,
   CircularProgress,
   Select,
   FormControl,
@@ -17,35 +15,49 @@ import {
 
 export function ExpensesView({ fetchTransactions }) {
   const [transactions, setTransactions] = useState([]);
-  const searchParams = useSearchParams()
-  const router = useRouter()
-  let user = typeof window !== 'undefined' ? JSON.parse(localStorage.getItem("user")) : {};
+  const [loading, setLoading] = useState(false);
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  let user =
+    typeof window !== "undefined"
+      ? JSON.parse(localStorage.getItem("user"))
+      : {};
 
   useEffect(() => {
     getTransactions();
   }, []);
 
   const getTransactions = async () => {
-    const data = await fetchTransactions(searchParams.get('month'), user);
+    const data = await fetchTransactions(searchParams.get("month"), user);
     setTransactions(data);
   };
 
   return (
     <Container maxWidth="sm" style={{ marginTop: "50px" }}>
-        <Button
-          onClick={() => router.push('/home')}
-          variant="contained"
-          color="error"
-        >
-          Back
-        </Button>
+      <Button
+        onClick={() => router.push("/home")}
+        variant="contained"
+        color="error"
+      >
+        Back
+      </Button>
       <div
         style={{ marginTop: "20px", display: "flex", justifyContent: "center" }}
       >
-        {transactions.map((transaction) => (
-          <ExpenseCell key={transaction._id} expense={transaction} />
-        ))}
+        <Grid container spacing={2}>
+          {transactions.map((transaction) => (
+            <Grid item key={transaction._id} xs={12} sm={6} md={4} lg={3}>
+              <ExpenseCell expense={transaction} />
+            </Grid>
+          ))}
+        </Grid>
       </div>
+
+      {loading && (
+        <div>
+          <CircularProgress size="large" />
+        </div>
+      )}
     </Container>
   );
 }
